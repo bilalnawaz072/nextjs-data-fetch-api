aaa58a5de7643585169ea78a6a3ad9bb7665ed00
Muhammad Bilal <bilalnawaz072@gmail.com>
2023-08-26 13:10:20 +0500

    filter method in GET Request

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -4,16 +4,23 @@ import { user } from "../db";
 export async function GET() {
   try {
     // Fetch the user data from the database
-    const userData = user.map((person) => ({
-      id: person.id,
-      name: person.name,
-      // age: person.age,
-      // city: person.city,
-      // country: person.country
-    }));
+    // const userData = user.map((person) => ({
+    //   id: person.id,
+    //   name: person.name,
+    //   // age: person.age,
+    //   // city: person.city,
+    //   // country: person.country
+    // }));
+
+
+    const filterData =  user.filter((person) => {
+      return person.id == 1 
+      //      return person.age > 20 && person.city === "Karachi";
+     
+    })
 
     // Return the user data as a JSON response
-    return NextResponse.json(userData);
+    return NextResponse.json(filterData);
   } catch (error) {
     // Handle any errors that occur during the data retrieval
     console.error("Error retrieving user data:", error);
