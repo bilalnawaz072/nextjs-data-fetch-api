6a8ffc56bef15861b51ab04b1a75563742f23adc
Muhammad Bilal <bilalnawaz072@gmail.com>
2023-08-26 12:25:54 +0500

    db change

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,9 +1,9 @@
+
 import { NextResponse } from "next/server";
 import { user } from "./db";
 
-export function GET() {
-    //export default does not work in this context
-  const data = user;
-  console.log(data);
-  return NextResponse.json(data);
+export  function GET() {
+    const data=user;
+    console.log(data);
+    return NextResponse.json(data);
 }
