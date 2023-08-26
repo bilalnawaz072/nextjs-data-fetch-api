0226e40424654e38db0ce908cf407ba636637a53
Muhammad Bilal <bilalnawaz072@gmail.com>
2023-08-26 11:27:37 +0500

    export GET methd error resolved

---
+++ app/api/users/db.ts
@@ -0,0 +1,20 @@
+export const user = [
+    {
+        id: 1,
+        name: "Dr Bilal",
+        age: 35,
+        gender: "male"
+    },
+    {
+        id: 2,
+        name: "Dr Bilal",
+        age: 35,
+        gender: "male"
+    },
+    {
+        id: 3,
+        name: "Dr Bilal",
+        age: 35,
+        gender: "male"
+    }
+]
--- app/api/users/route.tsx
+++
@@ -1,6 +0,0 @@
-
-import { NextResponse } from "next/server";
-
-export async function GET() {
-    return  NextResponse.json({ name: 'Dr Bilal', age: 20 ,city: "Karachi", country: "Pakistan"},{status: 200});
-}
